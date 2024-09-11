22f852a101bb48d0166ca3cfac57dd2c0d204b2f
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-09-11 11:52:09 +0500

    full

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -43,7 +43,7 @@ const Category = () => {
         <input required type="text" name='fname' placeholder='fname' />
         <input type="text" name='lname' placeholder='lname' />
         <input type="text" name='job' placeholder='job' />
-        <input type="text" name='gender' placeholder='gender' />
+        <input required type="text" name='gender' placeholder='gender' />
         <input type="text" name='bio' placeholder='bio' />
         <button className='bg-[green]'>{createLoading ? "Loading... " : "Create"}</button>
       </form>
@@ -51,15 +51,15 @@ const Category = () => {
       {
         updateCategoryItem &&
         <div className='bg-[#00001148]  fixed h-[100%] py-4 px-[100px] w-[100%]'>
-          <div className=' w-[400px] top-0 left-0'>
-            <form className='top-0 left-0 flex flex-col gap-3 bg-white' onSubmit={handleUpdateCategory} action="">
+          <div className=' w-[400px] top-0  left-0'>
+            <form className='top-0 px-2 py-2 left-0 flex flex-col gap-3 bg-white' onSubmit={handleUpdateCategory} action="">
               <input defaultValue={updateCategoryItem.fname} type="text" name='fname' />
               <input defaultValue={updateCategoryItem.lname} type="text" name='lname' />
               <input defaultValue={updateCategoryItem.job} type="text" name='job' />
               <input defaultValue={updateCategoryItem.gender} type="text" name='gender' />
               <input defaultValue={updateCategoryItem.bio} type="text" name='bio' />
-              <button>Save</button>
-              <button onClick={() => setUpdateCategoryItem(null)}>Cancel</button>
+              <button className='w-[350px] bg-green-700'>Save</button>
+              <button className='w-[350px]' onClick={() => setUpdateCategoryItem(null)}>Cancel</button>
             </form>
           </div>
         </div>
@@ -72,16 +72,16 @@ const Category = () => {
         {
           data?.map((category) => (
             <div className='gap-4' key={category.id}>
-              <div className='bg-green-400 w-[250px]  rounded-[20px] py-[20px] px-[20px] text-center flex flex-col gap-1'>
+              <div className='shadow-2xl w-[250px]  rounded-[20px] py-[20px] px-[20px] text-center flex flex-col gap-1'>
                 {/* <img className='' src={category.fname === "Jonny" ? jonny :  && category.gender === "male"? conor : selena} alt="" /> */}
                 <img className='h-[130px] object-contain' src={category.fname === "Boburmirzo" ? bobur : category.gender === "male" ? conor : selena} alt="" />
-                <h5 className='text-yellow-200'><span className='text-green-300'>Fname</span> :{category.fname}</h5>
+                <h5 className=''>{category.fname}</h5>
                 <h5>{category.lname}</h5>
                 <h5>{category.job}</h5>
                 <h5>{category.gender}</h5>
                 <h5>{category.bio}</h5>
                 <button onClick={() => deleteCategory(category.id)}>Delete</button>
-                <button onClick={() => setUpdateCategoryItem(category)}>Edit</button>
+                <button className='bg-[yellowgreen]' onClick={() => setUpdateCategoryItem(category)}>Edit</button>
               </div>
 
             </div>
